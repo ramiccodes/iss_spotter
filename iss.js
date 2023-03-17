53d0b2697f4b04be15243944c2b6599a10e24922
Ramic <ramlag1819@gmail.com>
2023-03-16 17:10:21 -0700

    Fetched my IP address using the IPIFY API

---
+++ iss.js
@@ -0,0 +1,29 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+
+const request = require('request');
+
+const fetchMyIP = function(callback) { 
+  request('https://geo.ipify.org/api/v2/country,city?apiKey=at_6NsY5uiHn6muCVKw3rfP3ePOz4j6Y', (err, res, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    if (res.statusCode !== 200) {
+      const msg = `Status Code ${res.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    callback(err, data);
+  })
+}
+module.exports = { fetchMyIP };
